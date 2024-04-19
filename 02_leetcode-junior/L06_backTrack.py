a53c4577dd2e284de3f8ebba5e95237bcfaa3152
TingFeng-7 <44105496+TingFeng-7@users.noreply.github.com>
2024-04-19 11:40:13 +0800

    Update L06_backTrack.py
    add 79 单词搜索

--- 02_leetcode-junior/L06_backTrack.py
+++ 02_leetcode-junior/L06_backTrack.py
@@ -208,3 +208,18 @@ class Solution:
 
         recur(root, targetSum)
         return res
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        # 79 单词搜索，在mxn的二维矩阵中找到符合给定word的字符串，存在则返回True，反之返回False
+        def dfs(i, j, k):
+            if not 0 <= i < len(board) or not 0 <= j < len(board[0]) or board[i][j] != word[k]: return False
+            if k == len(word) - 1: return True
+            board[i][j] = ''
+            res = dfs(i + 1, j, k + 1) or dfs(i - 1, j, k + 1) or dfs(i, j + 1, k + 1) or dfs(i, j - 1, k + 1)
+            board[i][j] = word[k]
+            return res
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if dfs(i, j, 0): return True
+        return False
